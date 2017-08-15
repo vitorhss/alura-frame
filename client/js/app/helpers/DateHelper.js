0a172ef53f9e155bcb4458571b95d79d7771c653
Vitor Souza <vitorrhss@gmail.com>
2017-08-14 22:15:18 -0300

    Utilizando template string e expressão regular

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,14 +1,18 @@
 class DateHelper{
 
+    
+    static dateToText(data){
+        return  `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+    }
+
     static textToDate(text){
 
-        return new Date(...text.split('-').map((item, indice) => item - indice % 2));
+        if(!/\d{4}-\d{2}-\d{2}/.test(text)){
+            throw Error('Data deve estar no formato yyyy-mm-dd');
+        }
+
+        return new Date(...text.split('-')
+                        .map((item, indice) => item - indice % 2));
     }
 
-    static dateToText(data){
-        return  data.getDate()
-        + '/' + (data.getMonth() + 1 )
-        + '/' + data.getFullYear();
-
-    }
 }
