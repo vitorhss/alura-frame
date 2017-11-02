7a753734e2d29ff32c532b1a2225f9e580dbd2da
Vitor Souza <vitor.souza@b2wdigital.com>
2017-11-02 16:11:10 -0200

    implementando herança e a view de mensagens

---
+++ client/js/app/views/MensagemView.js
@@ -0,0 +1,12 @@
+class MensagemView extends View {
+
+    constructor(elemento){
+        super(elemento);
+    }
+
+    _template(model){
+        return `<p class="alert alert-info"> ${model.texto} </p>`;
+    }
+
+  
+}
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,8 +1,9 @@
- class NegociacoesView{
+ class NegociacoesView extends View{
 
-     constructor(elemento){
-         this._elemento = elemento;
-     }
+    constructor(elemento){
+        super(elemento);
+    }
+
      _template(model){
          return `
             <table class="table table-hover table-bordered">
@@ -33,10 +34,6 @@
              <td> ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)} </td>
             </tfoot>
         </table>`;
-     }
-
-     update(model){
-         this._elemento.innerHTML = this._template(model);
      }
  }
  
